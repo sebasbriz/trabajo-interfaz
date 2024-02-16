626afca53d221311adedab956f82a61de312e767
sebasbriz <sebasbrizuela2003@gmail.com>
2024-02-16 20:06:51 -0300

    se agrego el video de fondo al formulario de registro y se agrego al menu inicial la opcion de iniciar sesion

--- registro.js
+++ registro.js
@@ -1,14 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(){
     //zona segura
-    reproducirVideo();
+    const registro = document.getElementById("btnRegistro");
+    const volver = document.getElementById("btnVolver");
+
+    //Se le asigna el evento click al botón crear
+    btnRegistro.addEventListener("click", function() {
+        capturarDatosDelFormulario();
+    });
 });
 
-function reproducirVideo() {
-    // Hay que permitir en google chrome la reproduccion de audio
-    // en el candadito
-    const video = document.getElementById("backVideo");
-    video.loop = true;
-    video.autoplay = true;
-    video.load();
-    video.play();
-}
+
